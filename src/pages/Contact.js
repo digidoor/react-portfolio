731b2af8c4ef1183e0f9d318c9e60d2407013367
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-14 15:51:47 -0700

    simple formatting improvements

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,12 @@
 const Contact = () =>
 {
 	return (
-		<div className="content">
+		<div className={`content contact`}>
 			<h1>Contact</h1>
+			<h3>The best way to contact me is to open an issue in any of my &nbsp;
+				<a href="https://github.com/digidoor?tab=repositories">github repos</a>.
+			</h3>
 		</div>);
-}
+};
  
 export default Contact;
