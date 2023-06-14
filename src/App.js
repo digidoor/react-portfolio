55676de33515b5a9ae728d448dc8e095af138442
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-14 05:58:23 -0700

    header now goes through navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 import Pokemon from './pages/Pokemon';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Navbar />
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/pokes' element={<Pokemon />} />
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import Navbar from './Navbar';
+const Header = () => {
+	return (<Navbar />);
+}
+ 
+export default Header;
