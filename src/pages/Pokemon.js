b23530d91e9e604885b2b805641b40463eccd33c
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-14 11:32:49 -0700

    styling updates, minor bug correct

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -15,7 +15,7 @@ const Pokemon = () =>
     		if(content) 
     			pokeFetch = await axios.get(`${url}${content}`);
 			else
-				pokeFetch = await axios.get(`${url}${Math.floor(Math.random()*1011)}`);
+				pokeFetch = await axios.get(`${url}${1+Math.floor(Math.random()*1010)}`);
 			console.log(pokeFetch.data);
     		setPokeData(pokeFetch.data);
     	} catch (error) { console.error(error); }
@@ -25,17 +25,17 @@ const Pokemon = () =>
 	
 	return( 
 		<div className="content">
-			<h1>Pokes</h1>
+			<h1 style={{marginBottom: 4 + 'px'}}>Pokes</h1>
 			<div className="pokemon">
 				{ pokeData ? (<img src={pokeData.sprites.front_default}></img>) : (<></>) }
+				<form onSubmit={getPoke}>
+					<input placeholder="Pokemon name..." value={content} onChange={ (event) => setContent(event.target.value)}/>
+					<button type="submit">Search</button>
+                	{/* {error && (<div>Something went wrong...</div>)} */}
+            	</form>
+            	<p style={{marginTop: 10 + 'px'}}>Enter a pokemon name or a pokedex number between 1 and 1010 to get a pokemon.</p>
+				<p>Leave blank for a random pokemon.</p>
 			</div>
-			<form onSubmit={getPoke}>
-				<input placeholder="Pokemon name..." value={content} onChange={ (event) => setContent(event.target.value)}/>
-				<button type="submit">Search</button>
-                {/* {error && (<div>Something went wrong...</div>)} */}
-            </form>
-            <p>Enter a pokemon name or a pokedex number between 1 and 1010 to get a pokemon.</p>
-			<p>Leave blank for a random pokemon.</p>
 		</div>
 	);
 };
