84e4491ebcceb6abd62e044d9aefb5dbc1b16f18
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-13 21:12:27 -0700

    data test that won't show up on deploy

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -5,8 +5,9 @@ const BlogList = ({blogs, title, deleter}) => {
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
-                    <p>Writen by { blog.author }</p>
-                    { deleter && <>&nbsp;<button onClick={()=> deleter(blog.id)}>Delete Blog</button> </>}
+                    <h5>Writen by { blog.author }</h5>
+                    <p>{ blog.body }</p>
+                    {/* { deleter && <>&nbsp;<button onClick={()=> deleter(blog.id)}>Delete Blog</button> </>} */}
                 </div>
             ))}
         </div>
