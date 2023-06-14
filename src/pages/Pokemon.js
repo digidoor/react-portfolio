57a69deb4d3506bda91121ceff5b609a45c9b323
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-14 07:19:51 -0700

    pokemon page up and running well

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,27 +1,41 @@
 import { useState } from 'react';
 import axios from 'axios';
-const url = "https://pokeapi.co/api/v2/pokemon/jolteon";
-
+const url = "https://pokeapi.co/api/v2/pokemon/";
 const Pokemon = () =>
 {
 	const [pokeData, setPokeData] = useState(null);
-	async function getPoke()
+	const [content, setContent] = useState("");
+	async function getPoke(event)
 	{
+		event.preventDefault();
+		console.log(content);
     	try
     	{
-    		var pokeFetch = await axios.get(url);
-			console.log(pokeFetch);
-    		setPokeData(pokeFetch);
+    		var pokeFetch;
+    		if(content) 
+    			pokeFetch = await axios.get(`${url}${content}`);
+			else
+				pokeFetch = await axios.get(`${url}${Math.floor(Math.random()*1011)}`);
+			console.log(pokeFetch.data);
+    		setPokeData(pokeFetch.data);
     	} catch (error) { console.error(error); }
-			
+		setContent("");	
 		return pokeFetch;
 	}
-	
-	getPoke();
 	
 	return( 
 		<div className="content">
 			<h1>Pokes</h1>
+			<div className="pokemon">
+				{ pokeData ? (<img src={pokeData.sprites.front_default}></img>) : (<></>) }
+			</div>
+			<form onSubmit={getPoke}>
+				<input placeholder="Pokemon name..." value={content} onChange={ (event) => setContent(event.target.value)}/>
+				<button type="submit">Search</button>
+                {/* {error && (<div>Something went wrong...</div>)} */}
+            </form>
+            <p>Enter a pokemon name or a pokedex number between 0 and 1010 to get a pokemon.</p>
+			<p>Leave blank for a random pokemon.</p>
 		</div>
 	);
 };
