b4378b552f0cb8f6bf3b57e5f825ba3b9589d7d7
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-13 22:07:04 -0700

    home link working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
+import { Link } from "react-router-dom";
 const Navbar = () =>
 {
     return (
         <nav className="navbar">
             <h1> Portfolio </h1>
             <nav className="link">
-                <ul></ul>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
                 <a href="/create" style={{
                     color:"white",
                     backgroundColor: "#46b392",
