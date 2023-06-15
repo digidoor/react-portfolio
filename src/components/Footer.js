6ddd48d3a4ada1b2e8712ce98736ba88c64f3645
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-15 06:39:13 -0700

    can't get color to work, prolly from * selector bs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,12 @@ import { FaGithub } from 'react-icons/fa';
 //import { FaGithubAlt } from 'react-icons/fa';
 
 const Footer = () => {
-	//var name  = "fab fa-github";
-	
-	//-brands fa-github fa-2xs
+
 	return (
 		<div className="footer">
 			<h6>This page was written by digidoor.</h6>
 			<a href="https://github.com/digidoor">
-				<FaGithub />
+				<FaGithub color="white" />
 			</a>
 		</div>
 	);
