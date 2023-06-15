73d93625697891b0626af91cef0c12d94945244d
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-15 01:02:35 -0700

    icons works via .css cdn fetch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
 const Footer = () => {
+	var name  = "fab fa-github";
+	
+	
 	return (
 		<div className="footer">
 			<h6>This page was written by digidoor.</h6>
-			<a href="https://github.com/digidoor">My github.</a>
+			<a href="https://github.com/digidoor">
+				<i className={name}></i>
+			</a>
 		</div>
 	);
 }
