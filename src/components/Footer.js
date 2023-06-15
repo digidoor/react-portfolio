9f83d51e989465e991d118b6651e44cbb9bcdba8
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-15 06:34:15 -0700

    use react-icons for icons instead of FontAwesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
+import { FaGithub } from 'react-icons/fa';
+//import { FaGithubAlt } from 'react-icons/fa';
+
 const Footer = () => {
-	var name  = "fab fa-github";
+	//var name  = "fab fa-github";
 	
-	
+	//-brands fa-github fa-2xs
 	return (
 		<div className="footer">
 			<h6>This page was written by digidoor.</h6>
 			<a href="https://github.com/digidoor">
-				<i className={name}></i>
+				<FaGithub />
 			</a>
 		</div>
 	);
